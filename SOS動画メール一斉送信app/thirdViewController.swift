72b622531f911d8ea44f97d40aea4ae3193976a5
Takashi Onoyama <cetaka1973@gmail.com>
2016-11-17 23:45:02 +0800

    SOS first time

---
+++ SOS動画メール一斉送信app/thirdViewController.swift
@@ -0,0 +1,13 @@
+//
+//  thirdViewController.swift
+//  SOS動画メール一斉送信app
+//
+//  Created by 小野山　隆 on 2016/11/17.
+//  Copyright © 2016年 Takashi Onoyama. All rights reserved.
+//
+
+import UIKit
+
+class thirdViewController: UICollectionViewCell {
+    
+}
