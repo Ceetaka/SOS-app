ee8834340aaefc671a0b44ea07ebb861ece6fe83
Takashi Onoyama <cetaka1973@gmail.com>
2016-11-17 20:18:05 +0800

    Initial Commit

---
+++ SOS動画メール一斉送信app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SOS動画メール一斉送信app
+//
+//  Created by 小野山　隆 on 2016/11/17.
+//  Copyright © 2016年 Takashi Onoyama. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
