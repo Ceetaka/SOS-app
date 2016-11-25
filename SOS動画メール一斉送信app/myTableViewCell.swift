09816049d1c19c6031d449a45a1b41c9e5645ac1
Takashi Onoyama <cetaka1973@gmail.com>
2016-11-26 01:32:48 +0800

    making Label1,2 in theTableViewSections

---
+++ SOS動画メール一斉送信app/myTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  myTableViewCell.swift
+//  SOS動画メール一斉送信app
+//
+//  Created by 小野山　隆 on 2016/11/26.
+//  Copyright © 2016年 Takashi Onoyama. All rights reserved.
+//
+
+import UIKit
+
+class myTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
