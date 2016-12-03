9001b4f852ef9cb2ed0be09dd0cc02b8eb36f579
Takashi Onoyama <cetaka1973@gmail.com>
2016-12-04 01:28:03 +0800

    making syokisettei

---
+++ SOS動画メール一斉送信app/myPrivacyItems3ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  myPrivacyItems3ViewController.swift
+//  SOS動画メール一斉送信app
+//
+//  Created by 小野山　隆 on 2016/12/04.
+//  Copyright © 2016年 Takashi Onoyama. All rights reserved.
+//
+
+import UIKit
+
+class myPrivacyItems3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
